81f8c1c0ee2585ca244c0d8948fb101a57b73f6c
Douangmany <aomaommanyvanh@gmail.com>
2022-08-04 00:34:34 +0700

    first commit

---
+++ src/Components/ImageData.js
@@ -0,0 +1,18 @@
+const ImageData = [
+  {
+    title: "flowers",
+    image:
+      "https://cdn.pixabay.com/photo/2022/05/31/18/12/flowers-7233987_960_720.jpg",
+  },
+  {
+    title: "natural",
+    image:
+      "https://cdn.pixabay.com/photo/2022/07/25/10/19/mountain-7343375_960_720.jpg",
+  },
+  {
+    title: "my cat",
+    image:
+      "https://cdn.pixabay.com/photo/2022/07/25/15/18/cat-7344042_960_720.jpg",
+  },
+];
+export default ImageData;
